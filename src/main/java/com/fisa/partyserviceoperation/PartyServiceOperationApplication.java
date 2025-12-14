eb885669c6605df90eade43283e64347ab6fc7cd
Matex2395 <mateorgafjo@gmail.com>
2025-12-13 19:23:35 -0500

    Initial Commit

---
+++ src/main/java/com/fisa/partyserviceoperation/PartyServiceOperationApplication.java
@@ -0,0 +1,13 @@
+package com.fisa.partyserviceoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyServiceOperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyServiceOperationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fisa/partyserviceoperation/PartyServiceOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa.partyserviceoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyServiceOperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
